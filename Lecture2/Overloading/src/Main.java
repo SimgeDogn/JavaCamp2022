216db06bb883a7fc8a586e2445d3820914c19e7c
SimgeDogn <114061020+SimgeDogn@users.noreply.github.com>
2022-09-26 01:39:59 +0300

    Create Main.java

---
+++ Lecture2/Overloading/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Operations operations = new Operations();
+		System.out.println(operations.add(2, 3));
+		System.out.println(operations.add(2, 3, 5));
+
+	}
+}
