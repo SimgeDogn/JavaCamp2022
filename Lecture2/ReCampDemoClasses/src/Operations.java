a38b70d6d81c2c3deef6988070ce7b1c8c205a30
SimgeDogn <114061020+SimgeDogn@users.noreply.github.com>
2022-09-26 00:55:40 +0300

    Create Operations.java

---
+++ Lecture2/ReCampDemoClasses/src/Operations.java
@@ -0,0 +1,17 @@
+
+public class Operations {
+	
+	public int Add(int num1,int num2) {
+		return num1+num2;
+	}
+	
+	public int Subs(int num1,int num2) {
+		return num1-num2;
+	}
+	public int Multi (int num1,int num2) {
+		return num1*num2;
+	}
+	public int Div (int num1,int num2) {
+		return num1/num2;
+	}
+}
