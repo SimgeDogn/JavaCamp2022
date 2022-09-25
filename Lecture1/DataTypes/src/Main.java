b411d402e5161ce678a11f97dae729702f3a8015
SimgeDogn <114061020+SimgeDogn@users.noreply.github.com>
2022-09-25 13:53:50 +0300

    Create Main.java

---
+++ Lecture1/DataTypes/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double num = 12.5;
+		
+		num = -129;
+		
+		
+		char character = 'A';
+		
+		
+		boolean isTrue = true;
+
+	}
+
+}
