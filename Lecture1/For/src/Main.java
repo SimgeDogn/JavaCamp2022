d793231c1d775f4455be7017e11da8bcd8386351
SimgeDogn <114061020+SimgeDogn@users.noreply.github.com>
2022-09-25 13:53:55 +0300

    Create Main.java

---
+++ Lecture1/For/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	 public static void main(String[] args) {
+		
+		for (int i = 2; i <=10; i+=2) {
+			System.out.println(i);
+		}
+		
+		System.out.println("For loop is over.");
+		
+		int i = 1;
+		
+		while(i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println("While loop is over.");
+		
+		int j = 1;
+		
+		do{
+			System.out.println(j);
+			j++;
+		}while(j <= 10);
+		
+		System.out.println("Do-While loop is over.");
+	}
+}
