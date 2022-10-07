fc312579311c347413a9fec037bd394bbfae90b9
SimgeDogn <114061020+SimgeDogn@users.noreply.github.com>
2022-10-08 00:54:16 +0300

    Create TeacherCreditManager.java

---
+++ Lecture3/YoutubeDemo/TeacherCreditManager.java
@@ -0,0 +1,7 @@
+
+public class TeacherCreditManager extends BaseCreditManager implements ICreditManager {
+	 public  void Calculate() {
+	        System.out.println("öğretmen kredisi hesaplandı");
+	    }
+
+}
